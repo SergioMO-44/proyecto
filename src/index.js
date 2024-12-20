53cfb9978cb746daa1d28facc008046f10ece426
Sergio Martínez <semarolmos@gmail.com>
2024-12-20 09:37:33 +0100

    proyecto intermolular

---
+++ src/index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+
+app.set("port", process.env.PORT || 8080);
+app.listen(app.set("port"), leerPuerto());
+//app.get("/rutaGet", function (req, res) {
+// console.log("hola en la consola de node, desde una llamada en el servidor");
+//res.send("hola desde una web llamada en el servidor");
+//});
+function leerPuerto() {
+  // console.log("hola desde el puerto" + app.get("port"));
+  // console.log("vamos a crear una ruta GET");
+  console.log(`hola desde el puerto: ${app.get("port")}`);
+}
+//app.get("/", function (req, res) {
+// console.log("Alguien ha accedido a la ruta get");
+// res.send("hola haciendo pruebas con la ruta get");
+//});
+//app.get("/cara", function (req, res) {
+// console.log("Alguien ha accedido al servidor con la cara");
+//res.send("hola haciendo pruebas con la ruta get en la cara");
+//});
+//app.get("/cruz", function (req, res) {
+//console.log("Alguien ha accedido al servidor con la cruz");
+//res.send("hola haciendo pruebas con la ruta get en la cruz");
+//});
+//app.get("/prueba", function (req, res) {
+//console.log("Alguien ha accedido a la ruta get, con la prueba");
+//res.send("hola haciendo pruebas con la ruta get, con la prueba");
+//});
+app.use(require("./routes/index")); //Routes
---
+++ src/routes/index.js
@@ -0,0 +1,28 @@
+const { Router } = require("express");
+const router = Router();
+
+router.get("/rutaGetConRouter", function (req, res) {
+  res.send("hola desde una web llamada en el servidor utilizando routes");
+});
+module.exports = router;
+
+router.get("/rutaGet", (req, res) => {
+  console.log(
+    "hola en la consola de node, desde la 1º ruta Get en el servidor"
+  );
+  res.send("hola soy la 1º ruta GET");
+});
+router.get("/rutaGetsegunda", (req, res) => {
+  console.log(
+    "hola en la consola de node, desde la 2º ruta Get en el servidor"
+  );
+  res.send("hola soy la 2º ruta GET");
+});
+router.get("/rutaGetconJSON", (req, res) => {
+  console.log(
+    "hola en la consola de node, desde la 3º ruta Get en el servidor"
+  );
+  res.json({
+    Title: "hola mundo con express probando cosas para subir a github",
+  });
+});
